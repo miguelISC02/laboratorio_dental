abe2939f2e8f3802ad8bdcf27af48224c996e064
miguelISC02 <miguelisc02@gmail.com>
2026-08-11 00:12:50 -0600

    Nuevos diseños

--- sanity/laboratorio-dental/sanity.cli.ts
+++ sanity/laboratorio-dental/sanity.cli.ts
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'mtuiqusbrw7pvq28e1w842s1',
     autoUpdates: true,
   }
 })
